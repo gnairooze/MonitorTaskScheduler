495cd51cacd7d5b72078871372b05a6c75d14aaf
George Nairooze <george.nairooze@linkdev.com>
2023-08-12 12:05:08 +0300

    update schema sql script

--- code/TaskSchedulerBusiness/MonitorTaskScheduler-DbSchema.sql
+++ code/TaskSchedulerBusiness/MonitorTaskScheduler-DbSchema.sql
@@ -12,8 +12,8 @@ BEGIN TRANSACTION;
 GO
 
 CREATE TABLE [Task] (
-    [HostName] nvarchar(450) NOT NULL,
-    [TaskName] nvarchar(450) NOT NULL,
+    [HostName] nvarchar(200) NOT NULL,
+    [TaskName] nvarchar(700) NOT NULL,
     [Next_Run_Time] nvarchar(max) NOT NULL,
     [Status] nvarchar(max) NOT NULL,
     [Logon_Mode] nvarchar(max) NOT NULL,
@@ -40,51 +40,29 @@ CREATE TABLE [Task] (
     [Repeat_Until_Time] nvarchar(max) NOT NULL,
     [Repeat_Until_Duration] nvarchar(max) NOT NULL,
     [Repeat_Stop_If_Still_Running] nvarchar(max) NOT NULL,
+    [Modified] datetime2 NOT NULL,
     CONSTRAINT [PK_Task] PRIMARY KEY ([HostName], [TaskName])
 );
 GO
 
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20230811134355_InitialCreate', N'7.0.10');
-GO
-
-COMMIT;
-GO
-
-BEGIN TRANSACTION;
-GO
-
-ALTER TABLE [Task] ADD [Modified] datetime2 NOT NULL DEFAULT '0001-01-01T00:00:00.0000000';
-GO
-
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20230811142625_ColumnModifiedAdded', N'7.0.10');
-GO
-
-COMMIT;
-GO
-
-BEGIN TRANSACTION;
-GO
-
-CREATE INDEX [IX_Task_Modified] ON [Task] ([Modified]);
-GO
-
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20230811143149_AddModifiedIndex', N'7.0.10');
-GO
-
-COMMIT;
-GO
-
-BEGIN TRANSACTION;
+CREATE TABLE [TaskChanges] (
+    [Id] bigint NOT NULL IDENTITY,
+    [TaskHistoryId] bigint NOT NULL,
+    [Created] datetime2 NOT NULL,
+    [HostName] nvarchar(200) NOT NULL,
+    [TaskName] nvarchar(700) NOT NULL,
+    [PropertyName] nvarchar(max) NOT NULL,
+    [OldValue] nvarchar(max) NOT NULL,
+    [NewValue] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_TaskChanges] PRIMARY KEY ([Id])
+);
 GO
 
 CREATE TABLE [TaskHistory] (
     [Id] bigint NOT NULL IDENTITY,
     [Created] datetime2 NOT NULL,
-    [HostName] nvarchar(max) NOT NULL,
-    [TaskName] nvarchar(max) NOT NULL,
+    [HostName] nvarchar(200) NOT NULL,
+    [TaskName] nvarchar(700) NOT NULL,
     [Next_Run_Time] nvarchar(max) NOT NULL,
     [Status] nvarchar(max) NOT NULL,
     [Logon_Mode] nvarchar(max) NOT NULL,
@@ -116,27 +94,7 @@ CREATE TABLE [TaskHistory] (
 );
 GO
 
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20230811202403_CreateTaskHistory', N'7.0.10');
-GO
-
-COMMIT;
-GO
-
-BEGIN TRANSACTION;
-GO
-
-CREATE TABLE [TaskChanges] (
-    [Id] bigint NOT NULL IDENTITY,
-    [TaskHistoryId] bigint NOT NULL,
-    [Created] datetime2 NOT NULL,
-    [HostName] nvarchar(450) NOT NULL,
-    [TaskName] nvarchar(450) NOT NULL,
-    [PropertyName] nvarchar(max) NOT NULL,
-    [OldValue] nvarchar(max) NOT NULL,
-    [NewValue] nvarchar(max) NOT NULL,
-    CONSTRAINT [PK_TaskChanges] PRIMARY KEY ([Id])
-);
+CREATE INDEX [IX_Task_Modified] ON [Task] ([Modified]);
 GO
 
 CREATE INDEX [IX_TaskChanges_Created] ON [TaskChanges] ([Created]);
@@ -149,7 +107,7 @@ CREATE INDEX [IX_TaskChanges_TaskName] ON [TaskChanges] ([TaskName]);
 GO
 
 INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20230812074052_AddTaskChanges', N'7.0.10');
+VALUES (N'20230812090116_InitialCreate', N'7.0.10');
 GO
 
 COMMIT;
